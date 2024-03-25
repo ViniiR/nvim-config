ca7401186cbac356b6ba84262d9d01813ef6b798
ViniiR <cvrm2004@gmail.com>
2024-03-25 12:12:01 -0300

    add linefeed, OS, tab and encoding to statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,10 +6,93 @@ M.ui = {
 	statusline = {
 		theme = "vscode_colored",
 		-- minimal, default, vscode, _colored
+		-- order = {
+		-- 	"mode",
+		-- 	"file",
+		-- 	"git",
+		-- 	"%=",
+		-- 	"lsp_msg",
+		-- 	"%=",
+		-- 	"diagnostics",
+		-- 	"%=",
+		-- 	"lsp",
+		-- 	"operatingSystem",
+		-- 	"lineFeed",
+		-- 	"cursor",
+		-- 	"cwd",
+		-- },
+		order = {
+			"mode",
+			"file",
+			"diagnostics",
+			"git",
+			"%=",
+			"lsp_msg",
+			"%=",
+			"operatingSystem",
+			"lineFeed",
+			"lsp",
+			"fileEncoding",
+			"tabLength",
+			"cursor",
+			"cwd",
+		},
+		separator_style = "dash",
+		modules = {
+			operatingSystem = function()
+				local bluehighlight = "%#CustomVBool#"
+				local linux = bluehighlight .. "󰣇 "
+				local windows = " "
+				local mac = " "
+				local osName = vim.loop.os_uname()
+				if string.len(osName.sysname) == 0 then
+					return ""
+				end
+				if osName.sysname == "Linux" then
+					return linux
+				elseif osName.sysname == "Windows_NT" then
+					return windows
+				elseif osName.sysname == "Darwin" then
+					return mac
+				end
+			end,
+			lineFeed = function()
+				local neutralhighlight = "%#St_Pos_txt#"
+				local linefeed = vim.bo.fileformat
+				if string.len(linefeed) == 0 then
+					return ""
+				end
+				if linefeed == "unix" then
+					return neutralhighlight .. "LF "
+				elseif linefeed == "dos" then
+					return "CRLF "
+				elseif linefeed == "mac" then
+					return "CR "
+				else
+					return "man you really are using a weird OS ngl"
+				end
+			end,
+			fileEncoding = function()
+				local encoding = vim.bo.fileencoding
+				local neutralhighlight = "%#St_Pos_txt#"
+				if string.len(encoding) == 0 then
+					return ""
+				end
+				return neutralhighlight .. string.upper(encoding) .. ", "
+			end,
+			tabLength = function()
+				local tablen = vim.bo.tabstop
+				local neutralhighlight = "%#St_Pos_txt#"
+				if string.len(tablen) == 0 then
+					return ""
+				end
+				return neutralhighlight .. "Tabs: " .. tablen .. ","
+			end,
+		},
 	},
-    telescope = {
-        style = 'bordered'
-    },
+	telescope = {
+		style = "bordered",
+	},
 	nvdash = {
 		load_on_startup = true,
 		-- header = {
@@ -22,19 +105,40 @@ M.ui = {
 		-- 	"▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
 		-- 	"█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
 		-- 	"    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
-		-- },
+		--},
 		header = {
-			"                                                                       ",
-			"                                                                     ",
-			"       ████ ██████           █████      ██                     ",
-			"      ███████████             █████                             ",
-			"      █████████ ███████████████████ ███   ███████████   ",
-			"     █████████  ███    █████████████ █████ ██████████████   ",
-			"    █████████ ██████████ █████████ █████ █████ ████ █████   ",
-			"  ███████████ ███    ███ █████████ █████ █████ ████ █████  ",
-			" ██████  █████████████████████ ████ █████ █████ ████ ██████ ",
-			"                                                                       ",
+			"     ▄▄         ▄ ▄▄▄▄▄▄▄    ",
+			"   ▄▀███▄     ▄██ █████▀     ",
+			"   ██▄▀███▄   ███            ",
+			"   ███  ▀███▄ ███            ",
+			"   ███    ▀██ ███            ",
+			"   ███      ▀ ███            ",
+			"   ▀██ █████▄▀█▀▄██████▄     ",
+			"     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀    ",
 		},
+		-- header = {
+		-- 	"                                                                       ",
+		-- 	"                                                                     ",
+		-- 	"       ████ ██████           █████      ██                     ",
+		-- 	"      ███████████             █████                             ",
+		-- 	"      █████████ ███████████████████ ███   ███████████   ",
+		-- 	"     █████████  ███    █████████████ █████ ██████████████   ",
+		-- 	"    █████████ ██████████ █████████ █████ █████ ████ █████   ",
+		-- 	"  ███████████ ███    ███ █████████ █████ █████ ████ █████  ",
+		-- 	" ██████  █████████████████████ ████ █████ █████ ████ ██████ ",
+		-- 	"                                                                       ",
+		-- },
+		-- updated = {
+		--     "                                                          ",
+		--     "    ███████  ███████  ███    ▄▄         ▄                 ",
+		--     "      ███      ███    ███  ▄▀███▄     ▄██                 ",
+		--     "      ███     ███     ███  ██▄▀███▄   ███                 ",
+		--     "      ███    ███      ███  ███  ▀███▄ ███                 ",
+		--     "      ███   ███       ███  ███    ▀██ ███                 ",
+		--     "      ███  ███        ███  ███      ▀ ███                 ",
+		--     "      ███████         ███  ▀██ █████▄▀█▀▄█                ",
+		--     "       █████          ███    ▀ ▀▀▀▀▀▀▀ ▀▀▀                ",
+		-- },
 
 		buttons = {
 			{ "  Find File", "<Space> f f", "Telescope find_files" },
@@ -47,6 +151,7 @@ M.ui = {
 	},
 	hl_override = {
 		["@punctuation.delimiter"] = { fg = "white" },
+		["CustomWhite"] = { fg = "white" },
 		["@property"] = { link = "CustomVVariable" },
 		["Label"] = { link = "CustomVVariable" },
 		["@punctuation.bracket"] = { link = "CustomVPurple" },
@@ -54,14 +159,25 @@ M.ui = {
 		["Boolean"] = { link = "CustomVBool" },
 		["@constant.builtin"] = { link = "CustomVBool" },
 		["@constant"] = { link = "CustomVBool" },
+		-- ["@keyword.storage"] = { link = "CustomVBool" },
 		["Type"] = { link = "CustomVType" },
+		["@type.builtin"] = { link = "CustomVType" },
 		["Function"] = { link = "CustomVFunction" },
 		["@function"] = { link = "CustomVFunction" },
 		["@function.call"] = { link = "CustomVFunction" },
 		["@function.builtin"] = { link = "CustomVFunction" },
+		-- ["@function.macro"] = { link = "@keyword.function" },
 		["@variable"] = { link = "CustomVVariable" },
 		["@variable.member"] = { link = "CustomVProperty" },
 		-- ["@spell"] = { guifg = "cyan" },
+		NvDashAscii = {
+			fg = "green",
+			bg = "none",
+		},
+		NvDashButtons = {
+			fg = "light_grey",
+			bg = "none",
+		},
 	},
 }
 
